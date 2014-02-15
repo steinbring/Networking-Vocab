20bdff0c06b20a549c60cadb31c91b822ea5855e
Joe Steinbring <joe@steinbring.net>
2014-02-15 17:52:10 -0600

    I finished a little more of the "introduction" part of the dictionary

--- js/dictionary.js
+++ js/dictionary.js
@@ -4,3 +4,43 @@ var arrIntroduction = [];
 arrIntroduction.push({ word: 'A+', definition: 'The professional certification established by CompTIA that verifies knowledge about PC operation, repair, and management.', slug: 'APlus' });
 // Item #2
 arrIntroduction.push({ word: 'Access Server', definition: 'See remote access server', slug: 'AccessServer' });
+// Item #3
+arrIntroduction.push({ word: 'Address', definition: 'A number that uniquely identifies each workstation and device on a network.  Without unique addresses, computers on the network could not reliably communicate.', slug: 'Address' });
+// Item #4
+arrIntroduction.push({ word: 'Address Management', definition: 'The process of centrally administering a finite number of network addresses for an entire LAN. Usually this task can be accomplished without touching the client workstations.', slug: 'AddressManagement' });
+// Item #5
+arrIntroduction.push({ word: 'Addressing', definition: 'The scheme for assigning a unique identifying number to every workstation and device on the network. The type of addressing used on a network depends on its protocols and network operating system.', slug: 'Addressing' });
+// Item #6
+arrIntroduction.push({ word: 'Asset Management', definition: 'The process of collecting and storing data on the number and types of software and hardware assets in an organization\'s network.  The data collection is automated by electronically examining each client from a server.', slug: 'AssetManagement' });
+// Item #7
+arrIntroduction.push({ word: 'Backbone', definition: 'The part of a network to which segments and significant shared devices (such as routers, switches, and servers) connect.  A backbone is sometimes referred to as "a network of networks" because of its role in interconnecting smaller parts of a LAN or WAN.', slug: 'Backbone' });
+// Item #8
+arrIntroduction.push({ word: 'Backing up', definition: 'The process of copying critical data files to a secure storage area.  Often backups are preformed according to a formulaic schedule.', slug: 'BackingUp' });
+// Item #9
+arrIntroduction.push({ word: 'CCIE (Cisco Certified Internetwork Expert)', definition: 'An elite certification that recognizes expert-level installation, configuration, management, and troubleshooting skills on networks that use a range of Cisco Systems\' devices', slug: 'CCIE' });
+// Item #10
+arrIntroduction.push({ word: 'CCNA (Cisco Certifies Network Associate)', definition: 'A professional certification that attests to one\'s skills in installing, configuring, maintaining, and troubleshooting medium-sized networks that use Cisco Systems\' switches and routers.' , slug: 'CCNA' });
+// Item #11
+arrIntroduction.push({ word: 'Certification', definition: 'The process of mastering material pertaining to a particular hardware system, operating system, programming language, or other software program, then proving your mastery by passing a series of exams.', slug: 'certification' });
+// Item #12
+arrIntroduction.push({ word: 'Client', definition: 'A computer on the network that requests resources or services from another computer on a network.  In some cases, a client could also act as a server.  The term client may also refer to the user of a client workstation or a client software application installed on the workstation.', slug: 'client' });
+// Item #13
+arrIntroduction.push({ word: '', definition: '', slug: '' });
+// Item #14
+arrIntroduction.push({ word: '', definition: '', slug: '' });
+// Item #15
+arrIntroduction.push({ word: '', definition: '', slug: '' });
+// Item #16
+arrIntroduction.push({ word: '', definition: '', slug: '' });
+// Item #17
+arrIntroduction.push({ word: '', definition: '', slug: '' });
+// Item #18
+arrIntroduction.push({ word: '', definition: '', slug: '' });
+// Item #19
+arrIntroduction.push({ word: '', definition: '', slug: '' });
+// Item #20
+arrIntroduction.push({ word: '', definition: '', slug: '' });
+// Item #21
+arrIntroduction.push({ word: '', definition: '', slug: '' });
+// Item #22
+arrIntroduction.push({ word: '', definition: '', slug: '' });
