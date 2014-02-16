320b5e78492f902b67a5a632018aeea1708047cb
Joe Steinbring <joe@steinbring.net>
2014-02-15 18:30:26 -0600

    I added to the dictionary

--- js/dictionary.js
+++ js/dictionary.js
@@ -1,3 +1,5 @@
+// This is a list of vocabulary words for use within the app.  I stored it as an array of structs simply because it was easy.
+
 // Define the introduction chapter
 var arrIntroduction = [];
 // Item #1
@@ -25,22 +27,38 @@ arrIntroduction.push({ word: 'Certification', definition: 'The process of master
 // Item #12
 arrIntroduction.push({ word: 'Client', definition: 'A computer on the network that requests resources or services from another computer on a network.  In some cases, a client could also act as a server.  The term client may also refer to the user of a client workstation or a client software application installed on the workstation.', slug: 'client' });
 // Item #13
-arrIntroduction.push({ word: '', definition: '', slug: '' });
+arrIntroduction.push({ word: 'Client/Server Architecture', definition: 'A network design in which client computers use a centrally administered server to share data, data storage space, and devices.', slug: 'ClientServerArchitecture' });
 // Item #14
-arrIntroduction.push({ word: '', definition: '', slug: '' });
+arrIntroduction.push({ word: 'Client/Server Network', definition: 'A network that uses centrally administered computers, known as servers, to enable resource sharing for and to facilitate communication between the other computers on the network.', slug: 'ClientServerNetwork' });
 // Item #15
-arrIntroduction.push({ word: '', definition: '', slug: '' });
+arrIntroduction.push({ word: 'CompTIA (Computing Technology Industry Association)', definition: 'An association of computer resellers, manufacturers, and training companies that sets industry-wide standards for computer professionals.  CompTIA established and sponsors the A+ and Network+ (Net+) certifications.', slug: 'CompTIA' });
 // Item #16
-arrIntroduction.push({ word: '', definition: '', slug: '' });
+arrIntroduction.push({ word: 'Connectivity Device', definition: 'One of several types of specialized devices that allows two or more networks or multiple parts of one network to connect and exchange data.', slug: 'ConnectivityDevice' });
 // Item #17
-arrIntroduction.push({ word: '', definition: '', slug: '' });
+arrIntroduction.push({ word: 'Convergence', definition: 'The use of data networks to carry voice (or telephone), video, and other communications services in addition to data.', slug: 'Convergence' });
 // Item #18
-arrIntroduction.push({ word: '', definition: '', slug: '' });
+arrIntroduction.push({ word: 'File Server', definition: 'A specialized server that enables clients to share applications and data across the network.', slug: 'FileServer' });
 // Item #19
-arrIntroduction.push({ word: '', definition: '', slug: '' });
+arrIntroduction.push({ word: 'File Services', definition: 'The functions of a file server that allow users to share data files, applications, and storage areas.', slug: 'FileServices' });
 // Item #20
-arrIntroduction.push({ word: '', definition: '', slug: '' });
+arrIntroduction.push({ word: 'Host', definition: 'A computer that enables resource sharing by other computers on the same network.', slug: 'Host' });
 // Item #21
-arrIntroduction.push({ word: '', definition: '', slug: '' });
+arrIntroduction.push({ word: 'Internet', definition: 'A complex WAN that connects LANs and clients around the globe.', slug: 'Internet' });
 // Item #22
-arrIntroduction.push({ word: '', definition: '', slug: '' });
+arrIntroduction.push({ word: 'Internet Services', definition: 'The services that enable a network to communicate with the Internet including Web servers and browsers, file transfer capabilities, Internet addressing schemes, security filters, and a means for directly logging on to other computers.', slug: 'InternetServices' });
+// Item #23
+arrIntroduction.push({ word: 'LAN (Local Area Network)', definition: 'A network of computers and other devices that is confined to a relatively small space, such as one building or even one office.', slug: 'LAN' });
+// Item #24
+arrIntroduction.push({ word: 'License Tracking', definition: 'The process of determining the number of copies of a single application that are currently in use on the network and whether the number in use exceeds the authorized number of licenses.', slug: 'LicenseTracking' });
+// Item #25
+arrIntroduction.push({ word: 'Load Balancing', definition: 'The process of distributing data transfer activity evenly so that no single device in overwhelmed.', slug: 'LoadBalancing' });
+// Item #26
+arrIntroduction.push({ word: 'Mail Server', definition: 'A server that manages the storage and transfer of e-mail messages.', slug: 'MailServer' });
+// Item #27
+arrIntroduction.push({ word: 'Mail Services', definition: 'The network services that manage the storage and tranfer of e-mail between users on a network.  In addition to sending, receiving, and storing mail, mail services can include filtering, routing, notification, scheduling, and data exchange with other mail servers.', slug: 'MailServices' });
+// Item #28
+arrIntroduction.push({ word: 'MAN (Metropolitan Area Network)', definition: 'A network that is larger than a LAN, typically connecting clients and servers from multiple buildings, but within a limited geographic area. For example, a MAN could connect multiple city government buildings around a city\'s center.', slug: 'MAN' });
+// Item #29
+arrIntroduction.push({ word: 'Management Services', definition: 'The network services that centrally administer and simplify complicated management tasks on the network. Examples of management services include license tracking, security auditing, asset management, address management, software distribution, traffic monitoring, load balancing, and hardware diagnosis.', slug: 'ManagementServices' });
+// Item #30
+arrIntroduction.push({ word: 'MCITP (Microsoft Certified IT Professional)', definition: 'A professional certification established by Microsoft that demonstrates in-depth knowledge about Microsoft products.', slug: 'MCITP' });
